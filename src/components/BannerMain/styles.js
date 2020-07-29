d99a56284ec2d08912ad1f930381bc84722ce8cd
Diego Nonato <nonatodiego@hotmail.com>
2020-07-29 18:50:35 -0300

    dia 02 - adicionando pagia de criacao de videos

--- src/components/BannerMain/styles.js
+++ src/components/BannerMain/styles.js
@@ -19,13 +19,15 @@ ContentAreaContainer.Item = styled.div`
   width: 50%;
   display: inline-block;
   margin-bottom: 50px;
+  font-weight: 600;
+  font-size: 24px;
   @media (max-width: 800px) {
     width: 100%;
   }
 `;
 
 ContentAreaContainer.Category = styled.h1`
-  font-family: Roboto;
+  font-family: 'Inconsolata', monospace;
   font-style: normal;
   font-weight: normal;
   font-size: 60px;
@@ -33,7 +35,7 @@ ContentAreaContainer.Category = styled.h1`
   display: flex;
   align-items: center;
   text-align: center;
-  display: inline-block; 
+  display: inline-block;
   padding: 25px;
   line-height: 1;
   border-radius: 4px;
@@ -53,7 +55,7 @@ ContentAreaContainer.Description = styled.p`
 
 ContentAreaContainer.Title = styled.h2`
   font-style: normal;
-  font-weight: 300;
+  font-weight: 700;
   font-size: 40px;
   line-height: 1;
   margin-top: 0;
@@ -69,7 +71,7 @@ export const BannerMainContainer = styled.section`
   height: 80vh;
   position: relative;
   color: #fff;
-  background-image: ${({ backgroundImage }) => `url(${backgroundImage})`}; 
+  background-image: ${({ backgroundImage }) => `url(${backgroundImage})`};
   background-size: cover;
   background-position: center;
   @media (max-width: 800px) {
@@ -79,7 +81,7 @@ export const BannerMainContainer = styled.section`
 
   &:after,
   &:before {
-    content: "";
+    content: '';
     display: block;
     position: absolute;
     left: 0;
@@ -91,7 +93,7 @@ export const BannerMainContainer = styled.section`
   &:before {
     top: 0;
     height: 100%;
-    background: rgba(0,0,0,0.5);
+    background: rgba(0, 0, 0, 0.5);
   }
 
   &:after {
@@ -101,7 +103,7 @@ export const BannerMainContainer = styled.section`
 `;
 
 export const WatchButton = styled.button`
-  font-family: 'Roboto', sans-serif;
+  font-family: 'Inconsolata', monospace;
   box-sizing: border-box;
   cursor: pointer;
   padding: 16px 24px;
@@ -116,7 +118,7 @@ export const WatchButton = styled.button`
   color: var(--black);
   background: var(--white);
   border-color: var(--black);
-  transition: opacity .3s;
+  transition: opacity 0.3s;
   display: none;
   margin: 0 auto;
   @media (max-width: 800px) {
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from './components/Menu';
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: '#141414' }}>
       <Menu />
@@ -14,7 +14,7 @@ function App() {
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={
-          'Rap bom é rap antigo? Rap e mídia combinam? Qual o futuro do Rap nacional? Ninguém melhor para debater essas perguntas que o próprio Rap. Desde os primeiros a se arriscar no estilo, até os mais recentes astros do underground. Um filme para os aspirantes à MC/DJ/produtor, fãs do gênero e interessados na cultura.'
+          'De 2016 pra cá, o consumo de trap no Spotify cresceu em média 61% ao ano. Nesse último ano, só os plays da Recayd Mob subiram quase 1000%. Todo mês, mais de 1 milhão de fãs ouve o som de artistas como Matuê, Raffa Moreira, Ebony, FBC e Sidoka. E por aí vai. Tem dúvida se o trap está bombando?'
         }
       />
 
@@ -35,4 +35,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
